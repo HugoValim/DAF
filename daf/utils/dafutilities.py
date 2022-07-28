ddea7849080c40bd48cfc73b821b9c0f0b110676
hugo.campos <hugo.campos@lnls.br>
2022-07-28 15:55:50 -0300

    Changed the place of some scans function to daf_scan_utils.py

--- daf/utils/dafutilities.py
+++ daf/utils/dafutilities.py
@@ -74,64 +74,6 @@ def sigint_handler_utilities(signum, frame):
 signal.signal(signal.SIGINT, sigint_handler_utilities)
 
 
-def get_passed_motor_order(sysargv):
-    """Function to pass the order that use choose to the scan_utils routine"""
-    all_possibilities = [
-        "-m",
-        "-e",
-        "-c",
-        "-p",
-        "-n",
-        "-d",
-        "--mu",
-        "--eta",
-        "--chi",
-        "--phi",
-        "--nu",
-        "--del",
-    ]
-
-    if PV_PREFIX == "EMA:B:PB18":
-        data = {
-            "mu": "huber_mu",
-            "eta": "huber_eta",
-            "chi": "huber_chi",
-            "phi": "huber_phi",
-            "nu": "huber_nu",
-            "del": "huber_del",
-        }
-    else:
-        data = {
-            "mu": "sol_m3",
-            "eta": "sol_m5",
-            "chi": "sol_m2",
-            "phi": "sol_m1",
-            "nu": "sol_m4",
-            "del": "sol_m6",
-        }
-
-    simp_to_comp = {
-        "mu": "mu",
-        "eta": "eta",
-        "chi": "chi",
-        "phi": "phi",
-        "nu": "nu",
-        "del": "del",
-        "m": "mu",
-        "e": "eta",
-        "c": "chi",
-        "p": "phi",
-        "n": "nu",
-        "d": "del",
-    }
-
-    motor_order = [
-        data[simp_to_comp[i.split("-")[-1]]] for i in sysargv if i in all_possibilities
-    ]
-
-    return motor_order
-
-
 class DevNull:
     """Supress errors for the user"""
 
