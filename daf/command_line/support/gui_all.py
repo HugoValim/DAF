f3a8de883f1a81ae3320dcaa24d47a4065830949
hugo.campos <hugo.campos@lnls.br>
2022-08-08 11:12:19 -0300

    Converting shell script functions into python

---
+++ daf/command_line/support/gui_all.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import argparse as ap
+import sys
+import os
+from os import path
+import yaml
+import subprocess
+
+from daf.command_line.support.support_utils import SupportBase
+import daf.utils.generate_daf_default as gdd
+import daf.utils.daf_paths as dp
+from daf.utils.log import daf_log
+
+
+class GUIAll(SupportBase):
+    DESC = """Opens all DAF's GUIs"""
+    EPI = """
+    Eg:
+       daf.guiall
+        """
+
+    def __init__(self):
+        self.parsed_args = self.parse_command_line()
+        self.parsed_args_dict = vars(self.parsed_args)
+
+    def parse_command_line(self) -> ap.Namespace:
+        super().parse_command_line()
+        args = self.parser.parse_args()
+        return args
+
+    @staticmethod
+    def open_daf_guis() -> None:
+        """If the --all option is passed open all DAF's GUIs as well"""
+        subprocess.Popen("daf.gui; daf.live", shell=True)
+
+    def run_cmd(self, arguments: dict) -> None:
+        self.open_daf_guis()
+
+
+@daf_log
+def main() -> None:
+    obj = GUIAll()
+    obj.run_cmd(obj.parsed_args_dict)
+
+
+if __name__ == "__main__":
+    main()
